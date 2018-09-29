c584cc03b48a071be450ae6c1905391cbc08a1ec
Will H <wandrewhouston@gmail.com>
2018-09-29 18:52:11 -0400

    linked database

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyDxlDO-v3tK1Fgzwriml1E816Nsq8XwAuk",
+    authDomain: "rps-multiplayer-5321a.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-5321a.firebaseio.com",
+    projectId: "rps-multiplayer-5321a",
+    storageBucket: "rps-multiplayer-5321a.appspot.com",
+    messagingSenderId: "966651762129"
+  };
+  firebase.initializeApp(config);
+
+  
