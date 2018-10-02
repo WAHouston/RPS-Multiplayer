936492795dd4c474e81fa0256bfe2d8da001eb27
Will H <wandrewhouston@gmail.com>
2018-10-02 16:40:04 -0400

    added onclick event to save display name in local storage

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,5 @@
-var config = {
-    apiKey: "AIzaSyDxlDO-v3tK1Fgzwriml1E816Nsq8XwAuk",
-    authDomain: "rps-multiplayer-5321a.firebaseapp.com",
-    databaseURL: "https://rps-multiplayer-5321a.firebaseio.com",
-    projectId: "rps-multiplayer-5321a",
-    storageBucket: "rps-multiplayer-5321a.appspot.com",
-    messagingSenderId: "966651762129"
-  };
-  firebase.initializeApp(config);
-
+$("#create-name").on("click", function(event) {
+  event.preventDefault()
+  localStorage.setItem("displayName", $("#display-name").val().trim())
+  $("#display-name").val("")
+})
